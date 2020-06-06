697d7330c0a366d4e6b6d0c94ed32144b34b50d0
Shraddha Shinde <shraddhashinde718@gmail.com>
2020-06-06 17:43:11 +0530

    code for master

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes, ROUTES } from "@angular/router";
+// import { CustomMaterialModule } from "./core/material.module";
+import { LayoutModule } from '@angular/cdk/layout';
+import { DatePipe } from '@angular/common';
+// import { EllipsisModule } from 'ngx-ellipsis';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { ChatComponent } from './chat/chat.component';
+import { RoomComponent } from './room/room.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RoomComponent, data: { title: 'Room Component' } },
+  { path: 'create-room', component: CreateRoomComponent, data: { title: 'Create Room Component' } },
+  { path: 'chat', component: ChatComponent, data: { title: 'Chat Component' } },
+
+
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    RoomComponent,
+    CreateRoomComponent
+  ],
+
+
+  imports: [ LayoutModule, FlexLayoutModule,
+    BrowserModule, AppRoutingModule,
+    BrowserAnimationsModule, FormsModule, ReactiveFormsModule, HttpClientModule,// HttpModule,
+    // RouterModule.forRoot(appRoutes) // <-- debugging purposes only
+    RouterModule.forRoot(appRoutes),
+    
+  ],
+  entryComponents: [],
+  bootstrap: [AppComponent],
+
+providers: [],
+})
+export class AppModule { }
