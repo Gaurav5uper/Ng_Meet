eea34ca3d7989b2f73db06f2cb220fa8b9b9f6f7
Shraddha Shinde <shraddhashinde718@gmail.com>
2020-06-06 20:27:05 +0530

    Home page UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,17 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ChatComponent } from './chat/chat.component';
 import { RoomComponent } from './room/room.component';
-import { CreateRoomComponent } from './create-room/create-room.component';
+import { CreateRoomComponent } from "./create-room/create-room.component";
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 const appRoutes: Routes = [
   { path: '', component: RoomComponent, data: { title: 'Room Component' } },
+  { path: 'room', component: RoomComponent, data: { title: 'Room Component' } },
   { path: 'create-room', component: CreateRoomComponent, data: { title: 'Create Room Component' } },
   { path: 'chat', component: ChatComponent, data: { title: 'Chat Component' } },
 
@@ -36,7 +43,16 @@ const appRoutes: Routes = [
 
   imports: [ LayoutModule, FlexLayoutModule,
     BrowserModule, AppRoutingModule,
-    BrowserAnimationsModule, FormsModule, ReactiveFormsModule, HttpClientModule,// HttpModule,
+    BrowserAnimationsModule, FormsModule,
+     ReactiveFormsModule,
+     HttpClientModule,
+     MatButtonModule,
+     MatCardModule,
+     
+     MatFormFieldModule,
+     
+     
+     // HttpModule,
     // RouterModule.forRoot(appRoutes) // <-- debugging purposes only
     RouterModule.forRoot(appRoutes),
     
